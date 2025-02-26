4be1777e2c6bdda113419e4992939dca80a7c0aa
Jason Lin <jclin2.2009@gmail.com>
2025-02-25 16:59:05 -0800

    calendarfetcher works!

--- app/index.tsx
+++ app/index.tsx
@@ -23,7 +23,7 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "~/components/ui/tooltip";
-import MainScreen from "~/components/MainScreen";
+import CalendarFetcher from "~/components/CalendarFetcher";
 
 const GITHUB_AVATAR_URI =
   "https://i.pinimg.com/originals/ef/a2/8d/efa28d18a04e7fa40ed49eeb0ab660db.jpg";
@@ -35,8 +35,8 @@ export default function Screen() {
     setProgress(Math.floor(Math.random() * 100));
   }
   return (
-    <View className="flex-1 justify-center items-center gap-5 p-6 bg-secondary/30">
-      <MainScreen />
+    <View className="flex-1 w-full h-full p-3">
+      <CalendarFetcher />
     </View>
   );
 }
