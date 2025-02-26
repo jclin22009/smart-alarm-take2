b3402e3484fadffc70f024d754b2a611e18f42c6
Jason Lin <jclin2.2009@gmail.com>
2025-02-25 19:07:08 -0800

    remove unused deps

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,5 @@
 import * as React from "react";
 import { View, ScrollView } from "react-native";
-import Animated, {
-  FadeInUp,
-  FadeOutDown,
-  LayoutAnimationConfig,
-} from "react-native-reanimated";
-import { Info } from "~/lib/icons/Info";
-import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
-import { Button } from "~/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
-import { Progress } from "~/components/ui/progress";
-import { Text } from "~/components/ui/text";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "~/components/ui/tooltip";
-import CalendarFetcher from "~/components/CalendarFetcher";
-import PodcastPlayer from "~/components/PodcastPlayer";
 import StartMyDay from "~/components/StartMyDay";
 import Alarm from "~/components/Alarm";
 
